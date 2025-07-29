abfc72d4e8b4281e7e133c76d4ad1e30778f1d6d
Mateusz <82820649+Mefju001@users.noreply.github.com>
2025-07-29 19:23:56 +0200

    update loginController&AuthService

--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.DTO.Request;
+using WebApplication1.Migrations;
 using WebApplication1.Services;
 using WebApplication1.Services.Interfaces;
 
@@ -28,13 +29,38 @@ namespace WebApplication1.Controllers
             {
                 return Unauthorized("Nieprawidłowe dane");
             }
-            return Ok(new { Token = token });
+            /*Response.Cookies.Append("refreshToken", refreshToken, new CookieOptions
+            {
+                Secure = true,
+                HttpOnly = true,
+                SameSite = SameSiteMode.Lax,
+                Expires = DateTime.Now.AddDays(7)
+            });
+            if (refreshToken is null)
+            {
+                throw new Exception("Refresh token is null");
+            }*/
+            return Ok(new { Message = "Zalogowano pomyślnie.", Token = token.token });
+
         }
         [AllowAnonymous]
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] UserRequest userRequest)
         {
             return Ok(await userServices.Register(userRequest));
+        }
+
+        [AllowAnonymous]
+        [HttpPost("refreshToken")]
+        public async Task<IActionResult> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+            if(refreshToken is null)
+            {
+                return Unauthorized();
+            }
+            await authService.RefreshAccessToken(refreshToken);
+            return Ok();
         }
     }
 }
